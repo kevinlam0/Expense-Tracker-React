9ea75b4fb8588324e89c4d8eb1c5fed698ed6f0b
kevinlam0 <kevinclam02@gmail.com>
2024-01-10 00:52:12 -0500

    separated expense and income

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map(transaction => transaction.amount);
+  const income = amounts
+    .filter(x => x > 0)
+    .reduce((acc, amount) => (acc += amount), 0)
+    .toFixed(2);
+  const expense = amounts
+    .filter(x => x < 0)
+    .reduce((acc, amount) => (acc += amount), 0)
+
   return (
     <div className='inc-exp-container'>
-        <div>
-            <h4>Income</h4>
-            <p className='money plus'>+$0.00</p>
-        </div>
-        <div>
-            <h4>Expense</h4>
-            <p className='money minus'>-$0.00</p>
-        </div>
+      <div>
+        <h4>Income</h4>
+        <p className='money plus'>+${income}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className='money minus'>-${Math.abs(expense).toFixed(2)}</p>
+      </div>
 
     </div>
   )
